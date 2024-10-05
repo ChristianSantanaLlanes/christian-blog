082b35950403971c5cd91eef2f8e3bc0dedbdfa4
ChristianSantanaLlanes <cs632706@gmail.com>
2024-10-04 20:01:16 -0500

    ...

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,9 @@
 <template>
   <UContainer>
     <h1 class="text-red-400 mt-5 text-2xl">Hello world</h1>
+    <div class="text-blue-300 text-lg mt-2">
+      Holaaaaaaaa
+    </div>
   </UContainer>
 </template>
 
